e525b6bc3b16fe3c917aaa816db6753c0fda3af2
Pierce Corcoran <code@thecodewarrior.dev>
2021-03-21 21:25:50 -0700

    Implement some PropertyScanner tests

--- core/src/test/kotlin/dev/thecodewarrior/prism/base/analysis/impl/PropertyScannerTest.kt
+++ core/src/test/kotlin/dev/thecodewarrior/prism/base/analysis/impl/PropertyScannerTest.kt
@@ -1,16 +1,44 @@
 package dev.thecodewarrior.prism.base.analysis.impl
 
 import dev.thecodewarrior.mirror.Mirror
+import dev.thecodewarrior.mirror.type.TypeMirror
+import dev.thecodewarrior.prism.annotation.Refract
+import dev.thecodewarrior.prism.annotation.RefractGetter
+import dev.thecodewarrior.prism.annotation.RefractImmutable
+import dev.thecodewarrior.prism.annotation.RefractMutable
 import dev.thecodewarrior.prism.testsupport.IdentityPrism
+import dev.thecodewarrior.prism.testsupport.IdentitySerializer
 import dev.thecodewarrior.prism.testsupport.assertInstanceOf
 import dev.thecodewarrior.reflectcase.ReflectTest
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
+@Suppress("LocalVariableName")
 internal class PropertyScannerTest: ReflectTest(
     "dev.thecodewarrior.prism.annotation.*"
 ) {
+    /**
+     * Asserts that a property is properly configured. Returns a callback suitable for [assertAll].
+     */
+    inline fun <reified T: ObjectProperty<*>> assertProperty(
+        actual: ObjectProperty<*>,
+        name: String,
+        immutable: Boolean,
+        type: TypeMirror? = null,
+        serializer: IdentitySerializer? = null
+    ): () -> Unit {
+        return {
+            assertInstanceOf<T>(actual)
+            assertEquals(name, actual.name)
+            assertEquals(immutable, actual.isImmutable)
+            if (type != null)
+                assertEquals(type, actual.type)
+            if (serializer != null)
+                assertEquals(serializer, actual.serializer)
+        }
+    }
+
     // # Basics
     @Test
     fun `a class with no @Refract annotated fields or methods should have no properties`() {
@@ -42,82 +70,344 @@ internal class PropertyScannerTest: ReflectTest(
         val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X)).sortedBy { it.name }
         assertEquals(2, properties.size)
         assertAll(
-            {
-                val property = assertInstanceOf<FieldProperty<*>>(properties[0])
-                assertEquals("immutable", property.name)
-                assertTrue(property.isImmutable)
-                assertEquals(Mirror.types.int, property.type)
-            },
-            {
-                val property = assertInstanceOf<FieldProperty<*>>(properties[1])
-                assertEquals("mutable", property.name)
-                assertFalse(property.isImmutable)
-                assertEquals(Mirror.types.int, property.type)
-            }
+            assertProperty<FieldProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<FieldProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
         )
     }
 
     @Test
     fun `getters and setters should be scanned to correctly-configured AccessorProperty properties`() {
+        val X by sources.add(
+            "X", """
+            class X {
+                @RefractGetter("immutable") int getImmutableField() { NOP; }
+                @RefractGetter("mutable") int getMutableField() { NOP; }
+                @RefractSetter("mutable") void setMutableField(int value) { NOP; }
+            }
+            """.trimIndent()
+        )
+        sources.compile()
+        // sort by name so they're in [immutable, mutable] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X)).sortedBy { it.name }
+        assertEquals(2, properties.size)
+        assertAll(
+            assertProperty<AccessorProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<AccessorProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+        )
     }
 
     @Test
     fun `kotlin properties with backing fields should be scanned to correctly-configured AccessorProperty properties`() {
+        class X {
+            @Refract("immutable")
+            val immutable: Int = 0
+
+            @Refract("mutable")
+            var mutable: Int = 0
+        }
+        sources.compile()
+        // sort by name so they're in [immutable, mutable] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java)).sortedBy { it.name }
+        assertEquals(2, properties.size)
+        assertAll(
+            assertProperty<AccessorProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<AccessorProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+        )
     }
 
     @Test
     fun `kotlin properties without backing fields should be scanned to correctly-configured AccessorProperty properties`() {
+        class X {
+            @Refract("immutable")
+            val immutable: Int
+                get() = 0
+
+            @Refract("mutable")
+            var mutable: Int
+                get() = 0
+                set(value) {}
+        }
+        sources.compile()
+        // sort by name so they're in [immutable, mutable] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java)).sortedBy { it.name }
+        assertEquals(2, properties.size)
+        assertAll(
+            assertProperty<AccessorProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<AccessorProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+        )
     }
 
     @Test
     fun `@JvmField kotlin properties should be scanned to correctly-configured FieldProperty properties`() {
+        class X {
+            @Refract("immutable")
+            @JvmField
+            val immutable: Int = 0
+
+            @Refract("mutable")
+            @JvmField
+            var mutable: Int = 0
+        }
+        sources.compile()
+        // sort by name so they're in [immutable, mutable] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java)).sortedBy { it.name }
+        assertEquals(2, properties.size)
+        assertAll(
+            assertProperty<FieldProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<FieldProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+        )
     }
 
     @Test
-    fun `directly annotated backing fields property should be scanned to correctly-configured FieldProperty properties`() {
+    fun `directly annotating a kotlin property's backing field should scan it to a correctly-configured FieldProperty`() {
+        class X {
+            @field:Refract("immutable")
+            val immutable: Int = 0
+
+            @field:Refract("mutable")
+            var mutable: Int = 0
+        }
+        sources.compile()
+        // sort by name so they're in [immutable, mutable] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java)).sortedBy { it.name }
+        assertEquals(2, properties.size)
+        assertAll(
+            assertProperty<FieldProperty<*>>(
+                properties[0],
+                name = "immutable",
+                immutable = true,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+            assertProperty<FieldProperty<*>>(
+                properties[1],
+                name = "mutable",
+                immutable = false,
+                type = Mirror.types.int,
+                serializer = IdentityPrism[Mirror.types.int].value
+            ),
+        )
     }
 
     @Test
     fun `all three property types should be able to coexist without conflicts`() {
+        class X {
+            @RefractGetter("accessor")
+            fun accessor(): Int = 0
+
+            @field:Refract("field")
+            val field: Int = 0
+
+            @Refract("property")
+            val property: Int = 0
+        }
+        sources.compile()
+        // sort by name so they're in [accessor, field, property] order
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java)).sortedBy { it.name }
+        assertEquals(3, properties.size)
+        assertAll(
+            assertProperty<AccessorProperty<*>>(
+                properties[0],
+                name = "accessor",
+                immutable = true,
+            ),
+            assertProperty<FieldProperty<*>>(
+                properties[1],
+                name = "field",
+                immutable = true,
+            ),
+            assertProperty<AccessorProperty<*>>(
+                properties[2],
+                name = "property",
+                immutable = true,
+            ),
+        )
     }
 
     // # @RefractImmutable
     @Test
     fun `adding '@RefractImmutable' to a non-final field should mark the resulting property as immutable`() {
+        val X by sources.add(
+            "X", """
+            class X {
+                @RefractImmutable @Refract("field") int field = 0;
+            }
+            """.trimIndent()
+        )
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X))
+        assertEquals(1, properties.size)
+        assertTrue(properties[0].isImmutable)
     }
 
     @Test
     fun `adding '@RefractImmutable' to a final field should throw an error`() {
+        val X by sources.add(
+            "X", """
+            class X {
+                @RefractImmutable @Refract("field") final int field = 0;
+            }
+            """.trimIndent()
+        )
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X))
     }
 
     @Test
     fun `adding '@RefractImmutable' to a kotlin 'var' should mark the resulting property as immutable`() {
+        class X {
+            @RefractImmutable
+            @Refract("property")
+            var property: Int = 0
+        }
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java))
+        assertEquals(1, properties.size)
+        assertTrue(properties[0].isImmutable)
     }
 
     @Test
     fun `adding '@RefractImmutable' to a kotlin 'val' should throw an error`() {
+        class X {
+            @RefractImmutable
+            @Refract("property")
+            val property: Int = 0
+        }
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java))
     }
 
     // # @RefractMutable
     @Test
     fun `adding '@RefractMutable' to a final field should mark the resulting property as mutable`() {
+        val X by sources.add(
+            "X", """
+            class X {
+                @RefractMutable @Refract("field") final int field = 0;
+            }
+            """.trimIndent()
+        )
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X))
+        assertEquals(1, properties.size)
+        assertFalse(properties[0].isImmutable)
     }
 
     @Test
     fun `adding '@RefractMutable' to a non-final field should throw an error`() {
+        val X by sources.add(
+            "X", """
+            class X {
+                @RefractMutable @Refract("field") int field = 0;
+            }
+            """.trimIndent()
+        )
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X))
     }
 
     @Test
     fun `adding '@RefractMutable' to a kotlin 'val' with a backing field should return a HybridMutableProperty`() {
+        class X {
+            @RefractMutable
+            @Refract("property")
+            val property: Int = 0
+        }
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java))
+        assertEquals(1, properties.size)
+        assertInstanceOf<HybridMutableProperty<*>>(properties[0])
+        assertFalse(properties[0].isImmutable)
     }
 
     @Test
     fun `adding '@RefractMutable' to a kotlin 'val' without a backing field should throw an error`() {
+        class X {
+            @RefractMutable
+            @Refract("property")
+            val property: Int
+                get() = 0
+        }
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java))
     }
 
     @Test
     fun `adding '@RefractMutable' to a kotlin 'var' should throw an error`() {
+        class X {
+            @RefractMutable
+            @Refract("property")
+            var property: Int = 0
+        }
+        sources.compile()
+        val properties = PropertyScanner.scan(IdentityPrism, Mirror.reflectClass(X::class.java))
     }
+
+    // adding both RefractMutable and RefractImmutable should throw an error
+
+    // static fields/methods should throw
+
+    // scanned properties should be alphabetical regardless of the originating class or underlying member name
 
     // # Empty names - should these be allowed?
     // a field with an empty name
